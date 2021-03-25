f753d3bd176ea1c264d7bd347ce046d4e6ac981e
poornesh-pH <poornesh9626@gmail.com>
2021-03-25 11:48:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Todo from "./todo.js";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Todo />
+    </div>
+  );
+}
