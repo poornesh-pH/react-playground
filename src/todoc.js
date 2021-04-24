72bc536a58f9d5ba66cc26f38eab782c463660e9
poornesh-pH <poornesh9626@gmail.com>
2021-04-24 15:04:51 +0530

    2 files modified

--- src/todoc.js
+++ src/todoc.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 
 export default class Todoc extends Component {
-  state = {
-    item: ""
-  };
   render() {
-    let addTodoo = e => {
-      e.preventDefault();
-      console.log(e.item)
+    let addTodoo =(e)=> {
+      event.preventDefault();
+      setItem(e)
     };
+ 
     return (
       <div>
         <p>Todo - Class Component</p>
-        <form onSubmit={addTodoo}>
+        <form onSubmit={()=>addTodoo()}>
           <input type="text" name="item" id="item" />
           <input type="submit" value="Submit"/>
         </form>
--- src/todof.js
+++ src/todof.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function Todo(){
-function addTodo(e){
-  e.preventDefault();
-console.log();
-}
-  return(
+export default function Todo() {
+  const [title, setTitle] = useState();
+  const [titlebool, setTitlebool] = useState(true);
+  const [again, setAgain] = useState("");
+  const [items, setItems] = useState([]);
+  const titleChange = () => {
+    setTitlebool(!titlebool);
+    console.log(titlebool);
+    if (titlebool === true) {
+      setTitle("using useState()");
+      setAgain("again");
+    } else {
+      setTitle("");
+      setAgain("");
+    }
+  };
+
+  function addTodo(e) {
+    e.preventDefault();
+    setItems();
+  }
+  return (
     <div>
-    <p>Todo component - Function component</p>
-    <br/>
-    <form onSubmit={addTodo}>
-    <input type="text" name="item" id="item" />
-    <input type="submit" value="Submit"/>
-    </form>
+      <p>Todo component - Function component {title}</p>
+      <button onClick={() => titleChange(item)}>Click me {again}</button>
+      <br />
+      <br />
+      <form onSubmit={addTodo}>
+        <input type="text" name="item" id="item" />
+        <input type="submit" value="Submit" />
+      </form>
+      {/* <p>{items}</p> */}
     </div>
-  ) 
+  );
 }
