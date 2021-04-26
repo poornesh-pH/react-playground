521c296996f99b4d43f5082995751f992e10818e
poornesh-pH <poornesh9626@gmail.com>
2021-04-26 18:47:33 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Todof from "./todof.js";
 import Todoc from "./todoc.js";
+import Playground from "./playground";
 export default function App() {
   return (
     <div>
       <h1>Hello React</h1>
       <Todof />
       <Todoc />
+      <Playground />
     </div>
   );
 }
---
+++ src/playground.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+
+export default function Playground() {
+  return (
+    <div>
+      <h1>Playground</h1>
+      {template}
+      {staticObj.name && <p>{staticObj.name}</p>}
+      {getTown(staticObj.town)}
+      {staticObj.age > 18 ? <p>{staticObj.age}</p> : "Under 18"}
+      {/* {staticArr} */}
+    </div>
+  );
+}
+
+var template = (
+  <div>
+    <p>Poornesh</p>
+    <p>Age: 23</p>
+    <p>Location : Sathy </p>
+  </div>
+);
+
+let staticObj = {
+  name: "Poornesh",
+  age: 23,
+  town: "Sathy"
+};
+
+let staticArr = ["Name 1", "Name 2", "Name 3"];
+
+function getTown(town) {
+  if (town) return <p>{town}</p>;
+  else return "Unknown";
+}
+
+const arrowFunction = (name, age) => {
+  return age ? name + " " + age : name;
+};
+console.log(arrowFunction("poornesh", 23));
+console.log(arrowFunction("abi"));
