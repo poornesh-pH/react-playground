42061f2a251bbfcf2cfd6d85da787a9f85f4aebc
poornesh-pH <poornesh9626@gmail.com>
2021-04-27 23:17:07 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./style.css";
 import Todof from "./todof";
 import Todoc from "./todoc";
@@ -11,27 +10,7 @@ export default function App() {
       <Todof />
       <Todoc />
       <Playground />
-      <h1>Indecision App</h1>
-      {options}
-      <form onSubmit={formOnSubmit}>
-        <input type="text" id="option" />
-        <button type="submit">Submit</button>
-      </form>
+      <div id="indecision" />
     </div>
   );
 }
-
-const options = [];
-
-const formOnSubmit = e => {
-  e.preventDefault();
-  options.push(e.target.elements.option.value);
-  e.target.elements.option.value = "";
-  console.log(options);
-};
-// function renderApp(){
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// }
-// renderApp();
